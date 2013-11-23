9863644073faf607ce0af0349bec24eb262b39bd
pulicucosta <pulicucosta@gmail.com>
2013-11-23 14:25:29 -0300

    comite - chamando segunda tela ec

--- NextBus10/src/pacote/NextBus10/MainActivity.java
+++ NextBus10/src/pacote/NextBus10/MainActivity.java
@@ -2,14 +2,28 @@ package pacote.NextBus10;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.principal_activity);
+		Button butao  = (Button) findViewById(R.id.button1);
+		butao.setOnClickListener(new OnClickListener() {
+								
+			@Override
+			public void onClick(View v) {
+
+				Intent inten1 = new Intent(MainActivity.this,Tempo_chegada.class);
+				startActivity(inten1);
+			}
+		 });
 	}
 
 	@Override
