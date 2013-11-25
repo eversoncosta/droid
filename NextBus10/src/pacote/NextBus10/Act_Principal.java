b94a79738d014f34b888ba7d4a70596b3187ecf1
pulicucosta <pulicucosta@gmail.com>
2013-11-25 00:19:54 -0200

    coordenadas

--- NextBus10/src/pacote/NextBus10/Act_Principal.java
+++ NextBus10/src/pacote/NextBus10/Act_Principal.java
@@ -1,15 +1,24 @@
 package pacote.NextBus10;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Act_Principal extends Activity {
+public class Act_Principal extends Activity implements LocationListener  {
 
+	private LocationManager locationManager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,13 +33,58 @@ public class Act_Principal extends Activity {
 				startActivity(inten1);
 			}
 		 });
+		
+		
+		EditText edit1 = (EditText) findViewById(R.id.editText1);
+/*		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    // Define the criteria how to select the provider
+	    Criteria criteria = new Criteria();
+	    criteria.setAccuracy(Criteria.ACCURACY_FINE);
+	    String provider = locationManager.getBestProvider(criteria, false);
+	    //Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	    Location location = locationManager.getLastKnownLocation(provider);
+	    */
+		
+	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				3000,   // 3 sec
+				10, this);
+		
 	}
+		
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+
+		 String str = "Latitude: "+location.getLatitude()+" \nLongitude: "+location.getLongitude();
+		Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
