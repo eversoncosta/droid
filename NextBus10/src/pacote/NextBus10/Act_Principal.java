1dd7be02052337df3601a1ec2688ec66bd47490b
pulicucosta <pulicucosta@gmail.com>
2013-11-27 00:19:25 -0200

    kskds

--- NextBus10/src/pacote/NextBus10/Act_Principal.java
+++ NextBus10/src/pacote/NextBus10/Act_Principal.java
@@ -1,6 +1,20 @@
 package pacote.NextBus10;
 
+import java.io.InputStream;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.R.layout;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -8,12 +22,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+
+
 
 public class Act_Principal extends Activity implements LocationListener  {
 
@@ -35,21 +56,28 @@ public class Act_Principal extends Activity implements LocationListener  {
 		 });
 		
 		
-		EditText edit1 = (EditText) findViewById(R.id.editText1);
-/*		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	    // Define the criteria how to select the provider
-	    Criteria criteria = new Criteria();
-	    criteria.setAccuracy(Criteria.ACCURACY_FINE);
-	    String provider = locationManager.getBestProvider(criteria, false);
-	    //Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	    Location location = locationManager.getLastKnownLocation(provider);
-	    */
 		
 	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
 				3000,   // 3 sec
 				10, this);
+		
+		String endereco = "";
+		
+		DefaultHttpClient httpClient = new DefaultHttpClient();    
+        URI uri;    
+        InputStream data = null;    
+        try {    
+            uri = new URI("http://10.0.2.2:8080/Next_Bus/WebGetOnibus?endereco="+endereco);    
+            HttpGet method = new HttpGet(uri);    
+            HttpResponse response = httpClient.execute(method);    
+            data = response.getEntity().getContent(); 
+            String teste = "";
+        } catch (Exception e) {    
+            e.printStackTrace();    
+        }    
+		
 		
 	}
 		
@@ -59,6 +87,11 @@ public class Act_Principal extends Activity implements LocationListener  {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void MultiplicarRadioButton()
+	{
+		Geocoder coder = new Geocoder(this);
 	}
 
 	@Override
